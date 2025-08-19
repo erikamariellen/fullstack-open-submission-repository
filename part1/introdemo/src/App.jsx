3d07922eb9debd57c14717ec5ab2bf689aa8155d
Erika Mariellen Ferreira <erikamariellen2011@gmail.com>
2025-08-19 12:08:08 -0300

    part 1d exercises 1.6-1.7

--- part1/introdemo/src/App.jsx
+++ part1/introdemo/src/App.jsx
@@ -1,30 +1,53 @@
-const Hello = (props) => {
+//conditional rendering
+import React from 'react'
 
+import { useState } from 'react'
+
+const History = (props) => {
   console.log(props)
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
   return (
     <div>
-      <p>
-
-        Hello {props.name}, you are {props.age} years old
-      </p>
+      button press history: {props.allClicks.join(' ')}
     </div>
   )
 }
+
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 
 const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
 
-  const name = 'Peter'
-  const age = 10
+
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'));
+    setRight(right + 1)
+  }
+
+  
 
   return (
     <div>
-      <h1>Greetings</h1>
-
-      <Hello name='Maya' age={26 + 10} />
-      <Hello name={name} age={age} />
+      {left}
+      <Button onClick={handleLeftClick} text='left' />
+      <Button onClick={handleRightClick} text='right' />
+      {right}
+      <History allClicks={allClicks} />
     </div>
   )
 }
-
 export default App
-
