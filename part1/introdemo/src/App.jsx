29c7de202eeb8fbcfc9ebbda76d437458e7f4ad4
Erika Mariellen Ferreira <erikamariellen2011@gmail.com>
2025-08-21 10:54:26 -0300

    exemples_part2

--- part1/introdemo/src/App.jsx
+++ part1/introdemo/src/App.jsx
@@ -1,64 +1,20 @@
-//conditional rendering
 import React from 'react'
 
-  if (props.good === 0 && props.neutral === 0 && props.bad === 0) {
-    return (
-      <div>
-        No feedback given
-      </div>
-    )
-  }
+const App = (props) => {
+  const { notes } = props
 
-
-
-
-import { useState } from 'react'
-
-const History = (props) => {
-  console.log(props)
-  if (props.allClicks.length === 0) {
-    return (
-      <div>
-        the app is used by pressing the buttons
-      </div>
-    )
-  }
   return (
     <div>
-      button press history: {props.allClicks.join(' ')}
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note => 
+          <li key={note.id} >
+            {note.content}
+          </li>
+        )}
+      </ul>
     </div>
   )
 }
 
-const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
-
-const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState([])
-
-
-
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
-    setLeft(left + 1)
-  }
-
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'));
-    setRight(right + 1)
-  }
-
-  
-
-  return (
-    <div>
-      {left}
-      <Button onClick={handleLeftClick} text='left' />
-      <Button onClick={handleRightClick} text='right' />
-      {right}
-      <History allClicks={allClicks} />
-    </div>
-  )
-}
 export default App
