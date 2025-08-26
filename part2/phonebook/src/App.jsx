f504319cb3e778cdd56facfaa0d9fa7ac8146b91
Erika Mariellen Ferreira <erikamariellen2011@gmail.com>
2025-08-26 10:43:45 -0300

    part 2b exercise 2.6

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -35,7 +35,7 @@ const App = () => {
       <h2>Numbers</h2>
         <>
           {persons.map((person) => 
-            <div>{person.name}</div>  
+            <div key={person.name}>{person.name}</div>  
           )}
         </>
     </div>
