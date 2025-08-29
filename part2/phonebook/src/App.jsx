c93e8b326ae4d36f684d19f5d949fcb055a1cce8
Erika Mariellen Ferreira <erikamariellen2011@gmail.com>
2025-08-28 21:27:00 -0300

    parte 2c exercise 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = ({filterName, handleFilterChange}) => (
       <div> 
@@ -35,12 +36,8 @@ const PersonForm = ({addPerson, newName, handleNameChange, newNumber, handleNumb
 )
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
+
   const [filterName, setFilterName] = useState('')
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
@@ -70,6 +67,16 @@ const App = () => {
       setNewNumber('')
   }
 
+  // Busca os dados do servidor apenas UMA vez
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
 
   return (
     <div>
